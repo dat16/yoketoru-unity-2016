8c9adb296108dc6c774f35d12cbe5c0267b8da03
YuTanaka <tanaka@am1.jp>
2017-02-03 11:56:16 +0900

    実装完了

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BGMManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private static BGMManager me = null; 
+	// ゲームオブジェクトを永続化
+	// @url http://qiita.com/srtkmsw/items/bf6a33d6bb2987c74936
+	void Awake() {
+		// 最初の1回のみ
+		if (me == null) {
+			me = this;
+			Debug.Log ("awake");
+			DontDestroyOnLoad (this);
+			AudioSource src = GetComponent<AudioSource> ();
+			src.Play ();
+		} else {
+			Debug.Log ("destroy");
+			Destroy (this);
+		}
 	}
 }
